2e3e87c47bb0218278c127f4b0bec64acb7ca650
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-27 15:47:10 -0500

    Working on adding auth to protect a page

---
+++ src/lib/storageCheck.js
@@ -0,0 +1,15 @@
+
+export default function storage() {
+  try {
+    // The problem was here
+    const authToken = localStorage.getItem('AUTH_TOKEN')
+    if (authToken !== null) {
+      return authToken;
+    }
+
+    return false;
+} catch (e) {
+    console.log(e.message);
+    return false;
+}
+}
--- src/lib/withData.js
+++ src/lib/withData.js
@@ -1,28 +1,41 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { setContext } from 'apollo-link-context';
+import storageCheck from './storageCheck';
+
 
 function createClient({ headers }) {
+  const token = storageCheck();
+
+  const authLink = setContext((_, { headers }) => {
+    const token = storageCheck();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })
+
   return new ApolloClient({
     // uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     uri: endpoint,
     clientState: {
-      defaults: {
-        loggedIn: false,
-      },
+      defaults: {},
       resolvers: {},
-      typeDefs: `
-      type Query {
-        loggedIn: Boolean
-      }
-    `,
+      typeDefs: ``,
     },
+    uri: endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
         },
-        headers,
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : '',
+        },
       });
     },
   });
