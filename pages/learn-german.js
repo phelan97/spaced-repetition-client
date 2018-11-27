1563a0d4aa1c42504894c3e4e448afe10600ef43
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-27 16:04:23 -0500

    Finished protecting learn-german page

--- pages/learn-german.js
+++ pages/learn-german.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 import LanguageCard from '../src/components/LanguageCard';
 import PleaseLogin from '../src/components/PleaseLogin';
+import Router from 'next/router';
+import storageCheck from '../src/lib/storageCheck';
+
 
 const LearnGerman = () => {
   return (
--- src/components/LanguageCard.js
+++ src/components/LanguageCard.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Error from './Errors';
+import { ApolloConsumer } from 'react-apollo';
+import Router from 'next/router'
+import storageCheck from '../lib/storageCheck';
 
-const LanguageCard = (props) => {
-  return (
-    <div>
-      <p>This is where the Language Card stuff is going to go.</p>
-      <p>Should look like props.germanWord, then input field, then if button is clicked, 
-        it shows the english word</p>
-      
-    </div>
-  );
-};
+class LanguageCard extends Component {
+  componentDidMount() {
+    const token = storageCheck();
+    
+    if (!token) {
+      return Router.push('/login');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is where the Language Card stuff is going to go.</p>
+        <p>Should look like props.germanWord, then input field, then if button is clicked, 
+          it shows the english word</p>
+        
+      </div>
+    );
+  };
+  
+}
 
 export default LanguageCard;
